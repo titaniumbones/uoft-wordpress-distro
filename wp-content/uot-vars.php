f7c98590b01357beb64b2eeeeb8d233160150965
Matt Price <matt.price@utoronto.ca>
2012-10-04 20:02:45 -0400

    pruning plugins list

--- wp-content/uot-vars.php
+++ wp-content/uot-vars.php
@@ -15,20 +15,20 @@ $PLUGINS=array(
                "cryptx" => '3.2.1',
                "custom-content-type-manager" => '0.9.6',
                "email-users" => '3.4.1',
-	       "fd-footnotes" => '',
-               "geo-mashup" => '1.4.1',
-               "grader" => '1.0',
+               "fd-footnotes" => '',
+               // "geo-mashup" => '1.4.1',
+               // "grader" => '1.0',
                "iframe-embed-for-youtube" => '1.0',
                "zotpress" => '4.5.4',
                "simple-taxonomy" => '3.3.1',
                //"featured-post-widget" => '3.1',
                "image-widget" => '3.2.8',
-               "import-users-from-csv" => '0.3.2',
-               "welcome-email-editor" => '3.4',
+               // "import-users-from-csv" => '0.3.2',
+               // "welcome-email-editor" => '3.4',
                "smooth-slider" => '2.3.5',
                "tinymce-advanced" => '3.4.5.1',
                "wp-db-backup" => '2.2.2',
-	       "youtube-embed" => '2.5.6'
+               "youtube-embed" => '2.5.6'
                );
 
 
