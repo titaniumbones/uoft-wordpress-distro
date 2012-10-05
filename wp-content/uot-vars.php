f09059ee6da538652d03195f04eab7f67520c30f
Matt Price <matt.price@utoronto.ca>
2012-10-04 22:24:20 -0400

    updated plugin versions

--- wp-content/uot-vars.php
+++ wp-content/uot-vars.php
@@ -11,23 +11,23 @@ $USEREMAIL='moptop99@gmail.com';
 // here is an array of plugins w/ version to load
 $PLUGINS=array(
                "attachments" => '1.6.2.1',
-               "contact-form-7" => '2.4.6',
-               "cryptx" => '3.2.1',
+               "contact-form-7" => '3.3',
+               "cryptx" => '3.2.2',
                "custom-content-type-manager" => '0.9.6',
-               "email-users" => '3.4.1',
+               "email-users" => '4.3.14',
                "fd-footnotes" => '',
                // "geo-mashup" => '1.4.1',
                // "grader" => '1.0',
                "iframe-embed-for-youtube" => '1.0',
                "zotpress" => '4.5.4',
-               "simple-taxonomy" => '3.3.1',
+               "simple-taxonomy" => '3.4.1',
                //"featured-post-widget" => '3.1',
-               "image-widget" => '3.2.8',
+               "image-widget" => '3.3.7',
                // "import-users-from-csv" => '0.3.2',
                // "welcome-email-editor" => '3.4',
-               "smooth-slider" => '2.3.5',
-               "tinymce-advanced" => '3.4.5.1',
-               "wp-db-backup" => '2.2.2',
+               "smooth-slider" => '2.4',
+               "tinymce-advanced" => '3.4.9',
+               "wp-db-backup" => '2.2.3',
                "youtube-embed" => '2.5.6'
                );
 
