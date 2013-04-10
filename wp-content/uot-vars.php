0218c44b1626889a0c2f9a58d98dc7b35970c5e8
Matt Price <matt.price@utoronto.ca>
2013-04-10 10:39:21 -0400

    add required+ foundation to distro.  Change some plugin version numbers.

--- wp-content/uot-vars.php
+++ wp-content/uot-vars.php
@@ -13,22 +13,20 @@ $PLUGINS=array(
                "attachments" => '1.6.2.1',
                "contact-form-7" => '3.3',
                "cryptx" => '3.2.2',
-               "custom-content-type-manager" => '0.9.6',
+               "custom-content-type-manager" => '0.9.7',
                "email-users" => '4.3.14',
                "fd-footnotes" => '',
                // "geo-mashup" => '1.4.1',
                // "grader" => '1.0',
-               "iframe-embed-for-youtube" => '1.0',
-               "zotpress" => '4.5.4',
+               "zotpress" => '5.0.7',
                "simple-taxonomy" => '3.4.1',
                //"featured-post-widget" => '3.1',
                "image-widget" => '3.3.7',
                // "import-users-from-csv" => '0.3.2',
                // "welcome-email-editor" => '3.4',
-               "smooth-slider" => '2.4',
+	       // "smooth-slider" => '2.4',
                "tinymce-advanced" => '3.4.9',
-               "wp-db-backup" => '2.2.3',
-               "youtube-embed" => '2.5.6'
+               "wp-db-backup" => '2.2.3'
                );
 
 
